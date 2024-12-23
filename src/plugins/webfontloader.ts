7c7961c65a5296c7dc8ce0d7d1604a64bc6cb84d
imsadegh <iamheydarpour@gmail.com>
2024-12-23 15:47:06 +0330

    Added persian font

--- src/plugins/webfontloader.ts
+++ src/plugins/webfontloader.ts
@@ -10,7 +10,9 @@ export async function loadFonts() {
   webFontLoader.load({
     google: {
       api: 'https://fonts.googleapis.com/css2',
-      families: ['Inter:wght@300;400;500;600;700;900&display=swap'],
+      // families: ['Inter:wght@300;400;500;600;700;900&display=swap'],
+      // families: ['Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'],
+      families: ['Vazirmatn:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'],
     },
   })
 }
