8608ff8ad9914afa0fdf65df3b8b87bfa8f28a5d
imsadegh <sadiq904@icloud.com>
2025-12-06 23:46:52 +0330

    refactor(account-settings): remove unused tabs and add i18n
    Remove billing, notifications, and connections tabs from the account settings page, retaining only account and security. Introduce internationalization support using vue-i18n for tab titles, making the component more locale-aware. This simplifies the UI and prepares for future scalability. Added corresponding translation keys in en.json.

--- src/pages/pages/account-settings/[tab].vue
+++ src/pages/pages/account-settings/[tab].vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import AccountSettingsAccount from '@/views/pages/account-settings/AccountSettingsAccount.vue'
-import AccountSettingsBillingAndPlans from '@/views/pages/account-settings/AccountSettingsBillingAndPlans.vue'
-import AccountSettingsConnections from '@/views/pages/account-settings/AccountSettingsConnections.vue'
-import AccountSettingsNotification from '@/views/pages/account-settings/AccountSettingsNotification.vue'
+// import AccountSettingsBillingAndPlans from '@/views/pages/account-settings/AccountSettingsBillingAndPlans.vue'
 import AccountSettingsSecurity from '@/views/pages/account-settings/AccountSettingsSecurity.vue'
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
 const route = useRoute('pages-account-settings-tab')
 
 const activeTab = computed({
@@ -13,13 +13,11 @@ const activeTab = computed({
 })
 
 // tabs
-const tabs = [
-  { title: 'Account', icon: 'ri-group-line', tab: 'account' },
-  { title: 'Security', icon: 'ri-lock-line', tab: 'security' },
-  { title: 'Billing & Plans', icon: 'ri-bookmark-line', tab: 'billing-plans' },
-  { title: 'Notifications', icon: 'ri-notification-3-line', tab: 'notification' },
-  { title: 'Connections', icon: 'ri-link', tab: 'connection' },
-]
+const tabs = computed(() => [
+  { title: t('accountSettings.account'), icon: 'ri-user-line', tab: 'account' },
+  { title: t('accountSettings.security'), icon: 'ri-lock-line', tab: 'security' },
+  // { title: t('accountSettings.billingPlans'), icon: 'ri-bookmark-line', tab: 'billing-plans' },
+])
 
 definePage({
   meta: {
@@ -30,29 +28,15 @@ definePage({
 
 <template>
   <div>
-    <VTabs
-      v-model="activeTab"
-      class="v-tabs-pill"
-    >
-      <VTab
-        v-for="item in tabs"
-        :key="item.icon"
-        :value="item.tab"
-        :to="{ name: 'pages-account-settings-tab', params: { tab: item.tab } }"
-      >
-        <VIcon
-          start
-          :icon="item.icon"
-        />
+    <VTabs v-model="activeTab" class="v-tabs-pill">
+      <VTab v-for="item in tabs" :key="item.icon" :value="item.tab"
+        :to="{ name: 'pages-account-settings-tab', params: { tab: item.tab } }">
+        <VIcon start :icon="item.icon" />
         {{ item.title }}
       </VTab>
     </VTabs>
 
-    <VWindow
-      v-model="activeTab"
-      class="mt-6 disable-tab-transition"
-      :touch="false"
-    >
+    <VWindow v-model="activeTab" class="mt-6 disable-tab-transition" :touch="false">
       <!-- Account -->
       <VWindowItem value="account">
         <AccountSettingsAccount />
@@ -63,20 +47,10 @@ definePage({
         <AccountSettingsSecurity />
       </VWindowItem>
 
-      <!-- Billing -->
-      <VWindowItem value="billing-plans">
+      <!-- Billing & Plans -->
+      <!-- <VWindowItem value="billing-plans">
         <AccountSettingsBillingAndPlans />
-      </VWindowItem>
-
-      <!-- Notification -->
-      <VWindowItem value="notification">
-        <AccountSettingsNotification />
-      </VWindowItem>
-
-      <!-- Connections -->
-      <VWindowItem value="connection">
-        <AccountSettingsConnections />
-      </VWindowItem>
+      </VWindowItem> -->
     </VWindow>
   </div>
 </template>
